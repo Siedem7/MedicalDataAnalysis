2575874f6fa33365d5024b01603f41f2ee9a45d9
Dave <dj300758@student.polsl.pl>
2023-11-23 21:30:39 +0100

    Added css file to /login path.

--- frontend/src/Login/login.tsx
+++ frontend/src/Login/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { loginForm } from '../Utils/ApiUtils'
+import './login.css'
 
 export default function Login() {
   const [username, setUsername] = React.useState('')
@@ -8,14 +9,21 @@ export default function Login() {
 
   return (
    <>
-    <h1>Login page</h1>
-    <form>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" name="username" value={username} onChange={(text) => setUsername(text.target.value)} />
-      <label htmlFor="password">Password</label>
-      <input type="password" id="password" name="password" value={password} onChange={(text) => setPassword(text.target.value)}  />
-    </form>
-    <button onClick={() => loginForm(username, password)}>Login</button>
+    <div className="container">
+      <form>
+        <div className="input-line">
+         <label htmlFor="username">Username</label>
+         <input type="text" id="username" name="username" value={username} onChange={(text) => setUsername(text.target.value)} />
+        </div>
+        <div className="input-line">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" value={password} onChange={(text) => setPassword(text.target.value)}  />
+        </div>
+      </form>
+      <div className="input-line">
+        <button onClick={() => loginForm(username, password)}>Login</button>
+      </div>
+    </div>
    </>     
   )
 
