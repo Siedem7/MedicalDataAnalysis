4f8b6726e4ad3bded685f96b2272d28b3a3d8b53
SzynS <kubasch0211@gmail.com>
2023-11-30 14:23:48 +0100

    create_user, delete_user and update_user fixed

--- backend/src/app.py
+++ backend/src/app.py
@@ -51,20 +51,103 @@ def create_app(database_uri="sqlite:///project.db"):
         if status != 200:
             abort(status, description=result)
 
+        expected_keys = ["login", "password", "group"]
+        if not all(key in request.json for key in expected_keys):
+            abort(400, description="Missing or incorrect keys in the request")
+
         login = request.json["login"]
         password = request.json["password"]
-        group_id = request.json["group"]
+        group_name = request.json["group"]
+
+        if not login or not password or not group_name:
+            abort(400, description="Invalid input data")
+        try:
+            group = db.session.execute(db.select(Group).filter_by(name=group_name)).scalar_one()
+        except NoResultFound:
+            abort(400, description="Invalid group name.")
+
         try:
             user = User(login=login,
                         password=hash_password(password),
                         password_expire_date=datetime.utcnow() + timedelta(days=30),
-                        group=group_id)
+                        group=group.id)
             db.session.add(user)
             db.session.commit()
         except IntegrityError:
             abort(409, description="User with that login already exists.")
 
         return jsonify("Successfully created user")
+
+
+    @app.route("/delete_user", methods=["DELETE"])
+    def delete_user():
+        token = request.headers.get("Authorization")
+        status, result = authorize_permissions(token, ["DELETE_USER_ACCOUNT"])
+
+        if status != 200:
+            abort(status, description=result)
+
+        if "id" not in request.json:
+            abort(400, description="Missing or incorrect keys in the request")
+
+        id = request.json["id"]
+        try:
+            user = db.session.execute(db.select(User).filter_by(id=id)).scalar_one()
+        except NoResultFound:
+            abort(404, description="User with that id not found.")
+        try:
+            db.session.delete(user)
+            db.session.commit()
+        except IntegrityError:
+            abort(409, description="Error while deleting the user.")
+
+        return jsonify("Successfully deleted user")
+
+    @app.route("/update_user", methods=["PUT"])
+    def update_user():
+        token = request.headers.get("Authorization")
+        status, result = authorize_permissions(token, ["UPDATE_USER_ACCOUNT"])
+        changed=0
+        if status != 200:
+            abort(status, description=result)
+
+        if "id" not in request.json:
+            abort(400, description="Missing or incorrect keys in the request")
+
+        id = request.json["id"]
+
+        try:
+            user = db.session.execute(db.select(User).filter_by(id=id)).scalar_one()
+        except NoResultFound:
+            abort(404, description="User with that id not found.")
+
+        if "group" in request.json:
+            group_name = request.json["group"]
+            try:
+                group = db.session.execute(db.select(Group).filter_by(name=group_name)).scalar_one()
+            except NoResultFound:
+                abort(400, description="Invalid group name.")
+
+            try:
+                user.group = group.id
+                db.session.commit()
+                changed=1
+            except IntegrityError:
+                abort(409, description="Error while updating the user.")
+
+        if "password" in request.json:
+            password = request.json["group"]
+            try:
+                user.password = hash_password(password)
+                user.password_expire_date=datetime.utcnow() + timedelta(days=30)
+                db.session.commit()
+                changed=1
+            except IntegrityError:
+                abort(409, description="Error while updating the user.")
+        if changed==0:
+            abort(400, description="Missing values")
+        return jsonify("Successfully updated user")
+
     return app
 
 
