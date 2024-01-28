19e32a319bca4a049b8b1bdd21df3eddd01c43fe
Michał Pilch <m.pilch2002@gmail.com>
2024-01-28 17:36:24 +0100

    fixed no env in tests

--- run_tests.bat
+++ run_tests.bat
@@ -1,6 +1,10 @@
 @echo off
 REM Description: Run tests
 
+REM Set path to the virtual environment and activate the virtual environment
+cd backend/venv/Scripts/
+call activate.bat
+cd ../../..
 
 REM Set path to test folder
 cd backend/tests
